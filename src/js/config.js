dc234c5f31cc9b644e9974e2f90803a02ccfaa43
Nhr98 <nirinasoa.herilanto@gmail.com>
2022-07-29 18:00:29 +0300

    app config

---
+++ src/js/config.js
@@ -0,0 +1,6 @@
+// STORE ALL VARIABLES ON THE APPLICATION, THE GOALS'RE TO MAKE EVERYTHING REUSABLE
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '4f75cf2b-5d2c-4bb4-856f-c0e4ec3fbef2';
+export const MODAL_CLOSE_SEC = 2.5;
