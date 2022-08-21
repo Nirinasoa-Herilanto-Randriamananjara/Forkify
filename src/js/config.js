2294ccaf3e6c2bf244789402f12f6f046786847e
Nhr98 <nirinasoa.herilanto@gmail.com>
2022-08-21 14:11:25 +0300

    add new config

--- src/js/config.js
+++ src/js/config.js
@@ -2,5 +2,6 @@
 export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes';
 export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
-export const KEY = '4f75cf2b-5d2c-4bb4-856f-c0e4ec3fbef2';
+export const API_KEY = '4f75cf2b-5d2c-4bb4-856f-c0e4ec3fbef2';
 export const MODAL_CLOSE_SEC = 2.5;
+export const CLOSE_TOAST_SEC = 2.5;
